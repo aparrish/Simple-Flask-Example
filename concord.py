f19beb51646d1c1bfcd4de8677385908076e4562
Adam Parrish <adam@decontextualize.com>
2012-04-08 18:43:34 -0400

    forgot os/port stuff, whoops!

--- concord.py
+++ concord.py
@@ -1,3 +1,5 @@
+import os
+
 from Concordance import Concordance
 from flask import Flask
 app = Flask(__name__)
@@ -18,5 +20,6 @@ if __name__ == '__main__':
 	for line in open('lovecraft.txt'):
 		line = line.strip()
 		concordance.feed(line)
-	app.run()
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
 
